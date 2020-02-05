4f8bdf8f7529c704db8d15d6ce1d2e3979b6a5ee
zagorGit <maja.zagorscak@nib.si>
2020-02-05 17:05:33 +0100

    Update combine_sequences.sh

--- scripts/combine_sequences.sh
+++ scripts/combine_sequences.sh
@@ -9,18 +9,18 @@ do
     line0+=$(head -1 "$file" | tail -1)
     line01=${line0// /_}
     str2=$(head -x "$file" | tail -1)
-    echo $line01 >> pre-miRNAs.xx # write string to a file
-    echo $str2 >> pre-miRNAs.xx
+    echo $line01 >> pre-miRNAs.fasta # write string to a file
+    echo $str2 >> pre-miRNAs.fasta
 
     line=$(head -x "$file" | tail -1) # read fifth line to a string
     str1=${line%% *} # split string by space
     str2="${str1//.}" # remove dots
-    echo $line01 >> mature-miRNAs.xx # write string to a file
-    echo $str2 >> mature-miRNAs.xx
+    echo $line01 >> mature-miRNAs.fasta # write string to a file
+    echo $str2 >> mature-miRNAs.fasta
 
     line=$(head -x "$file" | tail -1)
     str1=${line%% *}
     str2="${str1//.}"
-    echo $line01 >> star_miRNAs.xx # write string to a file
-    echo $str2 >> star_miRNAs.xx
+    echo $line01 >> star_miRNAs.fasta # write string to a file
+    echo $str2 >> star_miRNAs.fasta
 done
