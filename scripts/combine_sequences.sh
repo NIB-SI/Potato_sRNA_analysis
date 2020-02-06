af2d65e414765402a8c92236fe8772012518eae4
zagorGit <maja.zagorscak@nib.si>
2020-02-06 09:29:41 +0100

    Update combine_sequences.sh

--- scripts/combine_sequences.sh
+++ scripts/combine_sequences.sh
@@ -3,24 +3,30 @@
 # cat mature-miRNAs*.xx > mature-miRNAs.xx
 # cat star_miRNAs*.xx > star_miRNAs.xx
 
-for file in *.txt # for all files with .txt extension in folder
+touch ../output/pre-miRNAs.fasta
+touch ../output/mature_miRNAs.fasta
+touch ../output/star_miRNAs.fasta
+
+
+for file in ../input/MIRNAs/*.txt # for all files with .txt extension in folder
 do  
     line0=">"
     line0+=$(head -1 "$file" | tail -1)
     line01=${line0// /_}
     str2=$(head -3 "$file" | tail -1)
-    echo $line01 >> pre-miRNAs.fasta # write string to a file
-    echo $str2 >> pre-miRNAs.fasta
+    echo $line01 >> ../output/pre-miRNAs.fasta # write string to a file
+    echo $str2 >> ../output/pre-miRNAs.fasta
 
     line=$(head -5 "$file" | tail -1) # read fifth line to a string
     str1=${line%% *} # split string by space
     str2="${str1//.}" # remove dots
-    echo $line01 >> mature_miRNAs.fasta # write string to a file
-    echo $str2 >> mature_miRNAs.fasta
+    echo $line01 >> ../output/mature_miRNAs.fasta # write string to a file
+    echo $str2 >> ../output/mature_miRNAs.fasta
 
     line=$(head -6 "$file" | tail -1)
     str1=${line%% *}
     str2="${str1//.}"
-    echo $line01 >> star_miRNAs.fasta # write string to a file
-    echo $str2 >> star_miRNAs.fasta
+    echo $line01 >> ../output/star_miRNAs.fasta # write string to a file
+    echo $str2 >> ../output/star_miRNAs.fasta
 done
+
